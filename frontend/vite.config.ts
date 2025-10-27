6f271b58a734c5d2244ec9c1d915c032f78a42d2
sid-sg <sengupta.sid03@gmail.com>
2025-10-27 20:32:13 +0530

    added onnx & wasm files to webrtc frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/onnxruntime-web/dist/*.{wasm,mjs}',
+          dest: 'onnx-wasm'
+        }
+      ]
+    })
   ],
+  optimizeDeps: {
+    exclude: ['onnxruntime-web']
+  }
 })
