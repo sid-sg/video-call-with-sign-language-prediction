407f123f4991ee97bb90defc4ca742bca60ff5d0
sid-sg <sengupta.sid03@gmail.com>
2026-03-24 21:45:40 +0530

    changed ALLOWED_ORIGIN to * for local dev testing

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const CONFIG = {
   PORT: process.env.PORT ? Number(process.env.PORT) : 8080,
-  ALLOWED_ORIGIN: "https://webrtc-video-calling-demo.vercel.app",
-  // ALLOWED_ORIGIN: "*",
+  // ALLOWED_ORIGIN: "https://webrtc-video-calling-demo.vercel.app",
+  ALLOWED_ORIGIN: "*",
   METERED_API_KEY: process.env.METERED_API_KEY || "",
   TURN_API_URL: "https://webrtc-turn-server.metered.live/api/v1/turn/credentials",
 };
